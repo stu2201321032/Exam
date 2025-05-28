821edfc08830c29d82f516d7f58cdc2886a7b577
Antoan Romanov <121401441+stu2201321032@users.noreply.github.com>
2025-05-28 12:25:54 +0300

    Create Task.java

---
+++ task/Task.java
@@ -0,0 +1,13 @@
+package task;
+
+public class Task {
+    private final String description;
+
+    public Task(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
